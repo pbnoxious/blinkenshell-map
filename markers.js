a7e1f230bc86c6081350addfaf740348293db746
pbnoxious <pbnoxious@web.de>
2019-09-08 22:11:08 +0200

    First version of map for blinkenshell

---
+++ markers.js
@@ -0,0 +1,39 @@
+var infrastructure = {
+    "type": "FeatureCollection",
+    "features": [
+        {
+            "geometry": {
+                "type": "Point",
+                "coordinates": [
+		    18.09439,
+                    59.31079
+                ]
+            },
+            "type": "Feature",
+            "properties": {
+                "popupContent": "Triton"
+            },
+        }
+    ]
+};
+
+var users = {
+    "type": "FeatureCollection",
+    "features": [
+        {
+            "geometry": {
+                "type": "Point",
+                "coordinates": [
+		    8.65162,
+                    49.87253
+                ]
+            },
+            "type": "Feature",
+            "properties": {
+                "popupContent": "pbnoxious"
+            },
+        }
+    ]
+};
+
+

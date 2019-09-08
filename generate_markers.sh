86a4fb255ab93b447a767b994b038db306f752cc
Na <pbnoxious@triton.blinkenshell.local>
2019-09-08 22:11:08 +0200

    First version of map for blinkenshell

---
+++ generate_markers.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+coordfile='coordinates'
+markerfile='markers.js'
+rm -f $markerfile
+
+awk '{print "L.marker(["$2"], title: "$1").addTo(mymap).bindPopup(\""$1"\")"}' $coordfile > $markerfile

541d6deb5b9fd76584974a0815ba097bf2d4c252
pbnoxious <pbnoxious@web.de>
2019-09-09 11:11:02 +0200

    Removed old file

--- generate_markers.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-coordfile='coordinates'
-markerfile='markers.js'
-#rm -f $markerfile
-
-awk '{print "L.marker(["$2"], title: "$1").addTo(mymap).bindPopup(\""$1"\")"}' $coordfile > $markerfile

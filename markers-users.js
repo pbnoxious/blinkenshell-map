81a21f13cef0dd83f8e1bd9f790d68bb493ef852
pbnoxious <pbnoxious@blinkenshell.org>
2019-09-09 15:07:45 +0200

    Example markers in seperate file, seems to work now!

--- markers-users.js
+++ markers-users.js
@@ -12,37 +12,9 @@ var users = {
       "type": "Feature",
       "properties": {
         "user": "pbnoxious",
-        "popupContent": "You know where I live!"
+        "popupContent": "You know where I live!",
+        "color": "#cc0000"
       },
     },
-    {
-      "geometry": {
-        "type": "Point",
-        "coordinates": [
-          -40.71366,
-          32.87654
-        ]
-      },
-      "type": "Feature",
-      "properties": {
-          "user": "you?",
-          "popupContent": "some text to make you more interesting…"
-      },
-    },
-    {
-      "geometry": {
-        "type": "Point",
-        "coordinates": [
-          -59.0096,
-          -3.2423
-        ]
-      },
-      "type": "Feature",
-      "properties": {
-        "user": "Yacumama",
-        "popupContent": "Fear me, I can even define my own colour",
-        "color": "#8c6bb1"
-      },
-    }
   ]
 };

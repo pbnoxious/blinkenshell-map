c7613736e099ce8306924e2a14c85d38ed58f4ea
pbnoxious <pbnoxious@web.de>
2019-09-09 11:09:19 +0200

    Parser can now get files, wrinting is still missing

--- markers-users.js
+++ markers-users.js
@@ -1,50 +1,48 @@
 var users = {
-    "type": "FeatureCollection",
-    "features": [
-        {
-            "geometry": {
-                "type": "Point",
-                "coordinates": [
-		    8.65162,
-                    49.87253
-                ]
-            },
-            "type": "Feature",
-            "properties": {
-	        "user": "pbnoxious",
-                "popupContent": "You know where I live!"
-            },
-        },
-        {
-            "geometry": {
-                "type": "Point",
-                "coordinates": [
-		    -40.71366,
-                    32.87654
-                ]
-            },
-            "type": "Feature",
-            "properties": {
-		"user": "you?",
-                "popupContent": "some text to make you more interesting…"
-            },
-        },
-        {
-            "geometry": {
-                "type": "Point",
-                "coordinates": [
-		    -59.0096,
-                    -3.2423
-                ]
-            },
-            "type": "Feature",
-            "properties": {
-		"user": "Yacumama",
-                "popupContent": "Fear me, I can even define my own colour",
-		"color": "#8c6bb1"
-            },
-        }
-    ]
+  "type": "FeatureCollection",
+  "features": [
+    {
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          8.65162,
+          49.87253
+        ]
+      },
+      "type": "Feature",
+      "properties": {
+        "user": "pbnoxious",
+        "popupContent": "You know where I live!"
+      },
+    },
+    {
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -40.71366,
+          32.87654
+        ]
+      },
+      "type": "Feature",
+      "properties": {
+          "user": "you?",
+          "popupContent": "some text to make you more interesting…"
+      },
+    },
+    {
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -59.0096,
+          -3.2423
+        ]
+      },
+      "type": "Feature",
+      "properties": {
+        "user": "Yacumama",
+        "popupContent": "Fear me, I can even define my own colour",
+        "color": "#8c6bb1"
+      },
+    }
+  ]
 };
-
-

de03bd257a49118cd9ba184c405da9855b0a1896
pbnoxious <pbnoxious@web.de>
2021-07-21 11:51:30 +0200

    Removed second example marker

--- markers-example.js
+++ markers-example.js
@@ -14,21 +14,6 @@ var exampleMarkers = {
           "user": "you?",
           "popupContent": "some text to make you more interesting…"
       },
-    },
-    {
-      "geometry": {
-        "type": "Point",
-        "coordinates": [
-          -59.0096,
-          -3.2423
-        ]
-      },
-      "type": "Feature",
-      "properties": {
-        "user": "Yacumama",
-        "popupContent": "Fear me, I can even define my own colour",
-        "color": "#8c6bb1"
-      },
     }
   ]
 };
